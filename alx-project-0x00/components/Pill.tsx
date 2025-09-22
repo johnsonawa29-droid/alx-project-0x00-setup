1da7e89bc0535a5fb347e7c51f5a3f6f2b98001d
johnson <johnsonawa29@gmail.com>
2025-09-22 11:15:43 +0100

    yes

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,20 +1,12 @@
-import React from "react";
 
-interface PillProps {
-  title: string;
-}
+import React from "react";
+import { PillProps } from "@/interfaces";
 
 const Pill: React.FC<PillProps> = ({ title }) => {
   return (
     <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
       <p className="text-sm">{title}</p>
-
-    <div className="p-2 flex gap-2 mt-2">
-        <Pill title="Top Villa" />
-        <Pill title="Self CheckIn" />
-        <Pill title="Free Reschedule" />
- </div>
- </div>
+    </div>
   );
 };
 

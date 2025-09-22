d3ba6e2097c5686429de1c41e7c44ca53dcc1e8b
johnson <johnsonawa29@gmail.com>
2025-09-22 08:22:59 +0100

    yes

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -10,9 +10,8 @@ const Home: React.FC = () => {
       </Head>
       <main className="flex min-h-screen items-center justify-center bg-gray-100">
         <h1 className="text-3xl font-bold text-gray-900">
-          Welcome to ALX Project 0x01
-
-        </h1>Airbnb Application Clone system</h1>
+          Welcome to ALX Project 0x00
+          Airbnb Application Clone system</h1>
       </main>
 
   );

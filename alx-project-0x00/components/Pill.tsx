53ee63f02a72b582f32e4b619a3c2f7cb8802052
johnson <johnsonawa29@gmail.com>
2025-09-22 10:46:05 +0100

    yes

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -8,7 +8,13 @@ const Pill: React.FC<PillProps> = ({ title }) => {
   return (
     <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
       <p className="text-sm">{title}</p>
-    </div>
+
+    <div className="p-2 flex gap-2 mt-2">
+        <Pill title="Top Villa" />
+        <Pill title="Self CheckIn" />
+        <Pill title="Free Reschedule" />
+ </div>
+ </div>
   );
 };
 
